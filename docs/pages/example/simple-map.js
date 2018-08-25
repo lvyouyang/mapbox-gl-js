01b3aed6c7ab0fd61cc1278c8d53f5efb0fe7f56
Jing <jing.liu@mapbox.com>
2018-08-26 01:09:33 +0800

    Revert "Translate down 100%"

--- docs/pages/example/simple-map.js
+++ docs/pages/example/simple-map.js
@@ -1,8 +1,6 @@
 /*---
 title: Display a map
-title：地图展示
 description: Initialize a map in an HTML element with Mapbox GL JS.
-description：在HTML元素中用Mapbox GL JS将地图初始化。
 tags:
   - styles
 pathname: /mapbox-gl-js/example/simple-map/
